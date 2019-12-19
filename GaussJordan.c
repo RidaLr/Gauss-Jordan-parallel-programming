ee91be417a1528d658b8f14d3d7074cf50fdc1a3
RidaLr <32525321+RidaLr@users.noreply.github.com>
2019-12-19 17:59:26 +0100

    Add alloc_matrix and desalloc_matrix methods

--- GaussJordan.c
+++ GaussJordan.c
@@ -6,3 +6,26 @@
 void Init_Matrix(matrix mat){
 	
 }
+
+double **alloc_matrix(int lig, int col)
+{
+	double **mat=(double**)malloc(nl*sizeof(double *));
+	for( int i=0;i<nl;i++)
+	{
+		mat[i]=(double*)malloc(nc*sizeof(double));
+	}
+	
+	return mat;
+}
+
+void desalloc_matrix(t_matrice* mat)
+{
+	for(int i=0;i<=nl;i++)
+	{
+		free(mat[i]);	
+	}
+	
+	free(mat);
+	
+	return;
+}
