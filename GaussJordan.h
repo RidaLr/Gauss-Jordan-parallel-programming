052c99933b1909a3b8d68cd697f68c4b6c2f4e8c
RidaLr <32525321+RidaLr@users.noreply.github.com>
2019-12-19 18:04:32 +0100

    Add GaussJordanElimination  prototype

--- GaussJordan.h
+++ GaussJordan.h
@@ -6,6 +6,7 @@ double **alloc_matrice(int nlig,int ncol);
 void aff_matrice(double **mat);
 void saisir_matrix(double **mat);
 void desalloc_matrix(double **mat);
+double GaussJordanElimination (double **A, double *b);
 
 void Init_Matrix();
 void Gauss_Jordan();
