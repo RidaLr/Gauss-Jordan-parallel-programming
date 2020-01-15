52d2b31ab800b944f38f4c7643c4c3748fb3427f
RidaLr <32525321+RidaLr@users.noreply.github.com>
2020-01-15 08:21:02 +0100

    Update GaussJordan.c

--- GaussJordan.c
+++ GaussJordan.c
@@ -27,101 +27,25 @@ void desalloc_matrix(double **mat, int nl)
 	return;
 }
 
-/* Affichage du système */
+/* Show the system */
 void affich_systeme(double **A ,double *b, int size)
 {
 	int i , j ;
-	printf(" ===>Affichage du systeme : \n\n\n");
+	printf(" <======== Show the system =========>\n\n");
 	
 	for(i = 0 ; i < size ; i++)
 	{
-		printf("  (");
+		printf("  [");
 		for(j = 0 ; j < size ; j++)
 		{
-			printf("  %.3f  ",A[i][j]);
+			printf("  %f  ",A[i][j]);
 		}
-		printf(" )    (X%d)   =",i+1);
-		printf("\t%.3f",b[i]);
+		printf(" ]    [X%d]   =",i+1);
+		printf("\t %f",b[i]);
 		printf("\n\n");
 	}
 }
 
-
-/*
-void GaussJordanElimination(double **A, double *b){
-	int i,lignePivot,aux;
-	double *T, *T1, *T2;//Vecteur temporaire
-	
-	T = (double *) malloc (sizeof (double *) * N);
-	T1 = (double *) malloc (sizeof (double *) * N);
-	T2 = (double *) malloc (sizeof (double *) * N);
-	
-	
-	for(int k=0; k<N; k++)
-	{
-		if(A[k][k]!=0)
-		{
-			lignePivot = k;
-			printf("here %d %d\n",k,k);
-		}
-		else
-		{
-			i = k+1;
-			while(A[i][k]==0)
-			{
-				i+=1;
-			}
-			lignePivot = i;
-		}
-		//Permutation de la k-ème ligne avec la ligne de pivot de a matrice A
-		for(int i=k;i<N;i++)
-		{
-			T2[i]=A[k][i];
-			A[k][i]=A[lignePivot][i];
-			A[lignePivot][i]=T2[i];
-		}
-		//Permutaion de la k-ème ligne avec la ligne de pivot du vecteur b
-		aux=b[k];
-		b[k]=b[lignePivot];
-		b[lignePivot]=aux;
-		
-		//DIAGONALISATION DE LA MATRICE A
-		
-		
-		//La mise à jours de la matrice A
-		for(int i=k+1;i<N;i++)
-		{
-			printf("La mise à jours de la matrice A\n");
-			T[i]=-A[k][i]/A[k][k];
-		}
-		
-		for(int i=0;i<N;i++)
-		{
-			for(int j=k+1;j<N;j++){
-				A[i][j]=A[i][j]+T[j]*A[i][k];
-			}
-		}
-		for(int i=k+1;i<N;i++)
-		{
-			for(int j=k+1;j<N;j++){
-				A[i][j]=A[i][j]+T[j]*A[i][k];
-			}
-		}
-		//TRANSFORMATION DU VECTEUR b
-		 
-		//La mise à jours du vecteur b
-		for(int i=0;i<N;i++)
-		{
-			 T1[i]=A[i][k];
-			 aux = b[k];
-			 b[i]=b[i]-(T1[i]/A[k][k]);
-			 b[k]=aux;		
-		}
-		//affich_systeme(A ,b);
-		printf("jhgjhgj\n");
-	}
-	
-}*/
 
 //Sequential version
 void GaussJordanElim(double **A, double *b, int N)
