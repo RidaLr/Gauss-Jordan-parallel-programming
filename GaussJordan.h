cf1d74cb1a1508d0eaf76d3771d032b89ec73b09
RidaLr <32525321+RidaLr@users.noreply.github.com>
2020-01-18 00:09:08 +0100

    Update GaussJordan.h

--- GaussJordan.h
+++ GaussJordan.h
@@ -1,24 +1,23 @@
 #ifndef GAUSSJORDAN_H
 #define GAUSSJORDAN_H
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <omp.h>
 
 double **alloc_matrix(int nlig,int ncol);
 void aff_matrice(double **mat);
 void desalloc_matrix(double **mat, int nl);
 void GaussJordanElim(double **A, double *b,int N);
-void GaussJordanElimParallel(double **A, double *b, int N, int Nb_threads);
+void GaussJordanElimParallel(double **A, double *b, int N, int N_threads);
 void GaussJordanElimination(double **A, double *b);
 void ResulutionLinearSystem(double **D, double *y, int N);
-void ResulutionLinearSystemParallel(double **D, double *y, int N);
+void ResulutionLinearSystemParallel(double **D, double *y, int N, int N_threads);
 void affich_sol(double *x, int n);
 void affich_systeme(double **A ,double *b, int size);
 void saisie_mat(double **A, int n);
-void saisie_mat_alea(double **A, int N);
 void saisie_vect(double *b, int n);
 void saisie_vect_alea(double *b, int N);
-int getThreadsNbr();
-
-void Init_Matrix();
-void Show_Matrix();
+void saisie_mat_alea(double **A, int N);
 
 #endif
